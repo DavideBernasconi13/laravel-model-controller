a6f6e2b36c646598906a4795015b98ac52e8a013
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-05-22 16:32:36 +0200

    print in page movie and style footer

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -3,15 +3,22 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Movie; 
+use App\Models\Movie;
+
 class MovieController extends Controller
 {
-    public function index(){
-        $movies = Movie::all(); 
-        @dd($movies);  
-        //return view('movies.index');
+    public function index()
+    {
+        $movies = Movie::all();
+        //@dd($movies);  
+        return view('movies.index', compact('movies'));
     }
-    public function show($id){
+    public function show($id)
+    {
         //codice per recuperare il film
+        $movie = Movie::findOrFail($id);
+        //dd($movie);
+        return view('movies.show', compact('movie'));
+
     }
 }
--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,3 @@
-<div>footer</div>
+<footer class="bg-primary p-3">
+    <div class="text-center text-light">Created by DABE &copy 2024 </div>
+</footer>
